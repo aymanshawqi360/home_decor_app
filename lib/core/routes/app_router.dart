5d52442221e3ac106c8c7912a652c10132adaddf
aymanshawqi360 <aymanshawqi380gmail.com>
2025-05-27 01:38:07 +0300

    - added onboarding_screen. - added routes. - added home_decor_app

---
+++ lib/core/routes/app_router.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:home_decor_app/core/routes/routes.dart';
+import 'package:home_decor_app/features/onboarding_screen.dart';
+
+class AppRouter {
+  Route? generateRoute(RouteSettings routeSettings) {
+    switch (routeSettings.name) {
+      case Routes.onboarding:
+        return MaterialPageRoute(
+          builder: (_) {
+            return OnboardingScreen();
+          },
+        );
+
+      default:
+        return null;
+    }
+  }
+}
