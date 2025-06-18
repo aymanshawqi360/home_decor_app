b0481177aae9c50d2262b14541228d424a8ba711
aymanshawqi360 <aymanshawqi380gmail.com>
2025-06-18 21:42:08 +0300

    - just a modify

--- lib/features/home/presentation/pages/button_navigate_between_screen.dart
+++ lib/features/home/presentation/pages/button_navigate_between_screen.dart
@@ -6,7 +6,8 @@ import 'package:home_decor_app/core/di/dependency_injection.dart';
 import 'package:home_decor_app/core/helper/app_assets.dart';
 import 'package:home_decor_app/core/helper/spacing.dart';
 import 'package:home_decor_app/core/theme/colors.dart';
-import 'package:home_decor_app/features/home/presentation/best_seller/best_seller_cubit.dart';
+
+import 'package:home_decor_app/features/home/presentation/cubit/best_seller/best_seller_cubit.dart';
 import 'package:home_decor_app/features/home/presentation/cubit/categories/categories_cubit.dart';
 import 'package:home_decor_app/features/home/presentation/cubit/cubit/slider_cubit.dart';
 import 'package:home_decor_app/features/home/presentation/pages/home_screen.dart';
