6bad3c2091104ed0b3b36903ae11d6b79f5a975b
aymanshawqi360 <aymanshawqi380gmail.com>
2025-07-01 18:41:44 +0300

    added dependency injection

---
+++ lib/core/di/dependency_injection.dart
@@ -0,0 +1,17 @@
+import 'package:dio/dio.dart';
+import 'package:get_it/get_it.dart';
+import 'package:home_decor_app/core/newtorks/dio_factory.dart';
+import 'package:home_decor_app/features/login/data/api/login_api_service.dart';
+import 'package:home_decor_app/features/login/data/repo_implementation/repo_implementation.dart';
+import 'package:home_decor_app/features/login/domain/repo/login_repo.dart';
+import 'package:home_decor_app/features/login/domain/usecase/login_use_case.dart';
+
+final getIt = GetIt.instance;
+
+Future<void> setupGetIt() async {
+  Dio dio = DioFactory.getDio();
+  //Login
+  getIt.registerLazySingleton<LoginApiService>(() => LoginApiService(dio));
+  getIt.registerLazySingleton<LoginRepo>(() => RepoImplementation(getIt()));
+  getIt.registerLazySingleton<LoginUseCase>(() => LoginUseCase(getIt()));
+}
