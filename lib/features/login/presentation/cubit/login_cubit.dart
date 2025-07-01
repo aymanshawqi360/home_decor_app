4e4d6684bfcde599551734b8af991597afb20c1f
aymanshawqi360 <aymanshawqi380gmail.com>
2025-07-01 18:32:59 +0300

    added login cubit

---
+++ lib/features/login/presentation/cubit/login_cubit.dart
@@ -0,0 +1,44 @@
+import 'package:bloc/bloc.dart';
+import 'package:flutter/material.dart';
+import 'package:home_decor_app/core/newtorks/api_error_model.dart';
+import 'package:home_decor_app/core/newtorks/api_reuselte.dart';
+import 'package:home_decor_app/features/login/domain/entity/login_request_entity.dart';
+import 'package:home_decor_app/features/login/domain/entity/login_response.entity.dart';
+import 'package:home_decor_app/features/login/domain/usecase/login_use_case.dart';
+import 'package:home_decor_app/features/login/presentation/cubit/login_state.dart';
+
+class LoginCubit extends Cubit<LoginState> {
+  final LoginUseCase _loginUseCase;
+  LoginCubit(this._loginUseCase) : super(LoginInitial());
+  final controllerEmail = TextEditingController();
+  final controllerPassword = TextEditingController();
+  final formKey = GlobalKey<FormState>();
+  bool isPassword = false;
+  void getLogin() async {
+    emit(LoginLoading());
+    final response = await _loginUseCase.getLogin(
+      LoginRequestEntity(
+        passwrod: controllerPassword.text,
+        userName: controllerEmail.text,
+      ),
+    );
+    if (response is Success<LoginResponseEntity>) {
+      emit(LoginSuccess());
+    } else if (response is Failure<LoginResponseEntity>) {
+      emit(
+        LoginFailure(
+          errorMessage: ApiErrorModel(
+            errorMessage: response.errorMessage!.errorMessage,
+            password: response.errorMessage!.password,
+            detail: response.errorMessage!.detail,
+          ),
+        ),
+      );
+    }
+  }
+
+  void hidePasswordWord() {
+    isPassword = !isPassword;
+    emit(LoginHidePasswordWord(isPasswrod: isPassword));
+  }
+}
