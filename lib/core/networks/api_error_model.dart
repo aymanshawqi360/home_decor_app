9f11d533926421673bc900c4cfab7906256731f1
aymanshawqi360 <aymanshawqi380gmail.com>
2025-07-03 22:01:02 +0300

    Update api error model file

--- lib/core/networks/api_error_model.dart
+++ lib/core/networks/api_error_model.dart
@@ -1,4 +1,3 @@
-// ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:json_annotation/json_annotation.dart';
 
 part 'api_error_model.g.dart';
@@ -6,25 +5,21 @@ part 'api_error_model.g.dart';
 @JsonSerializable()
 class ApiErrorModel {
   String? errorMessage;
-  String? detail;
-  List? password;
-  Map<String, dynamic>? errors;
 
-  ApiErrorModel({this.errorMessage, this.errors, this.detail, this.password});
+  Map<String, dynamic>? errors;
+  ApiErrorModel({this.errors, this.errorMessage});
 
   factory ApiErrorModel.fromJson(Map<String, dynamic> json) =>
       _$ApiErrorModelFromJson(json);
 
   Map<String, dynamic> toJson() => _$ApiErrorModelToJson(this);
-
   String allSignUpError() {
     if (errors == null || errors!.isEmpty)
       return errorMessage ?? "Unknown error occurred";
+
     final message = StringBuffer();
     for (var data in errors!.entries) {
-      for (var d in data.value) {
-        message.writeln(d);
-      }
+      message.writeln(data.value);
     }
     return message.toString();
   }
