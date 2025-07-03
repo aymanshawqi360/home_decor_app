4dd369a77f2596868f7f917a405d19c934e9e21f
aymanshawqi360 <aymanshawqi380gmail.com>
2025-07-03 22:03:58 +0300

    update login jsonSerializable file

--- lib/core/networks/api_error_model.g.dart
+++ lib/core/networks/api_error_model.g.dart
@@ -8,16 +8,12 @@ part of 'api_error_model.dart';
 
 ApiErrorModel _$ApiErrorModelFromJson(Map<String, dynamic> json) =>
     ApiErrorModel(
-      errorMessage: json['errorMessage'] as String?,
       errors: json['errors'] as Map<String, dynamic>?,
-      detail: json['detail'] as String?,
-      password: json['password'] as List<dynamic>?,
+      errorMessage: json['errorMessage'] as String?,
     );
 
 Map<String, dynamic> _$ApiErrorModelToJson(ApiErrorModel instance) =>
     <String, dynamic>{
       'errorMessage': instance.errorMessage,
-      'detail': instance.detail,
-      'password': instance.password,
       'errors': instance.errors,
     };
