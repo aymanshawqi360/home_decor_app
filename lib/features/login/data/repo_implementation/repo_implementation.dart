1ec9a0d0c8857485dc810dc73eed0e4b53de6420
aymanshawqi360 <aymanshawqi380gmail.com>
2025-07-03 22:13:00 +0300

    update repo implementation file

--- lib/features/login/data/repo_implementation/repo_implementation.dart
+++ lib/features/login/data/repo_implementation/repo_implementation.dart
@@ -1,7 +1,7 @@
 import 'package:home_decor_app/core/networks/api_error_hundler.dart';
 import 'package:home_decor_app/core/networks/api_result.dart';
 import 'package:home_decor_app/features/login/data/api/login_api_service.dart';
-import 'package:home_decor_app/features/login/data/api/mapper/login_mapper.dart';
+import 'package:home_decor_app/features/login/data/mapper/login_mapper.dart';
 import 'package:home_decor_app/features/login/data/model/login_request_model.dart';
 import 'package:home_decor_app/features/login/data/model/login_response_model.dart';
 import 'package:home_decor_app/features/login/domain/entity/login_request_entity.dart';
@@ -20,13 +20,15 @@ class RepoImplementation extends LoginRepo {
     try {
       LoginResponseModel loginResponseModel = await _loginApiService.getLogin(
         LoginRequestModel(
-          passwrod: loginRequestEntity.passwrod,
+          password: loginRequestEntity.passwrod,
           userName: loginRequestEntity.userName,
         ),
       );
+
       LoginResponseEntity loginResponseEntity = LoginMapper.gotLoginMapper(
         loginResponseModel,
       );
+
       return ApiResulte.success(loginResponseEntity);
     } catch (failure) {
       return ApiResulte.failure(ApiErrorHandler.apiHundle(failure));
