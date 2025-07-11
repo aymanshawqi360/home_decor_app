618c391cee45128aba57abd67a2af63861a288cc
aymanshawqi360 <aymanshawqi380gmail.com>
2025-07-11 23:07:05 +0300

    added update to api constants

--- lib/core/networks/api_constants.dart
+++ lib/core/networks/api_constants.dart
@@ -2,4 +2,7 @@ enum Token { ACCESS_TOKEN, REFRESH_TOKEN }
 
 class ApiConstants {
   static const String baseUrl = "https://decor.up.railway.app/";
+  static const String baseUrlImage = "http://decor.up.railway.app";
+  static const double maxPrice = 1470.14;
+  static const double minPrice = 233.8;
 }
