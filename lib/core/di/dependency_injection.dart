4edeaa1828b01d3c285f959261d92e5a7adf36b7
aymanshawqi360 <aymanshawqi380gmail.com>
2025-07-11 23:04:53 +0300

    added a update of dependency injection

--- lib/core/di/dependency_injection.dart
+++ lib/core/di/dependency_injection.dart
@@ -8,8 +8,16 @@ import 'package:home_decor_app/features/home/domain/use_cases/best_seller_use_ca
 import 'package:home_decor_app/features/home/domain/use_cases/categories_use_cases.dart';
 import 'package:home_decor_app/features/home/domain/use_cases/new_collection_use_cases.dart';
 import 'package:home_decor_app/features/home/domain/use_cases/slider_use_cases.dart';
+import 'package:home_decor_app/features/login/data/api/login_api_service.dart';
+import 'package:home_decor_app/features/login/data/repo_implementation/login_repo_implementation.dart';
+import 'package:home_decor_app/features/login/domain/repo/login_repo.dart';
+import 'package:home_decor_app/features/login/domain/usecase/login_use_case.dart';
+import 'package:home_decor_app/features/search/data/api/search_api_service.dart';
+import 'package:home_decor_app/features/search/data/repo_implementation/search_repo_implementation.dart';
+import 'package:home_decor_app/features/search/domain/repo/search_repo.dart';
+import 'package:home_decor_app/features/search/domain/use_cases/search_use_cases.dart';
 import 'package:home_decor_app/features/sign_up_screen/data/api/sign_up_api_service.dart';
-import 'package:home_decor_app/features/sign_up_screen/data/repo_implementation/repo_implementation.dart';
+import 'package:home_decor_app/features/sign_up_screen/data/repo_implementation/login_repo_implementation.dart';
 import 'package:home_decor_app/features/sign_up_screen/domain/repo/sign_up_repo.dart';
 import 'package:home_decor_app/features/sign_up_screen/domain/use_cases/sign_up_use_cases.dart';
 
@@ -41,4 +49,19 @@ Future<void> setupGetIt() async {
   getIt.registerLazySingleton<SignUpApiService>(() => SignUpApiService(dio));
   getIt.registerLazySingleton<SignUpRepo>(() => RepoImplementation(getIt()));
   getIt.registerLazySingleton<SignUpUseCases>(() => SignUpUseCases(getIt()));
+
+  //Login
+
+  getIt.registerLazySingleton<LoginApiService>(() => LoginApiService(dio));
+  getIt.registerLazySingleton<LoginRepo>(
+    () => LoginRepoImplementation(getIt()),
+  );
+  getIt.registerLazySingleton<LoginUseCase>(() => LoginUseCase(getIt()));
+
+  //Search
+  getIt.registerLazySingleton<SearchApiService>(() => SearchApiService(dio));
+  getIt.registerLazySingleton<SearchRepo>(
+    () => SearchRepoImplementation(getIt()),
+  );
+  getIt.registerLazySingleton<SearchUseCases>(() => SearchUseCases(getIt()));
 }
