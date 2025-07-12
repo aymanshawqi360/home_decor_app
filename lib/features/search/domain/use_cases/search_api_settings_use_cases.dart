c6797fc7178bb58f12d22c9f99cd59acf97dbace
aymanshawqi360 <aymanshawqi380gmail.com>
2025-07-13 00:31:39 +0300

    added search api settings use cases to user cases folder

---
+++ lib/features/search/domain/use_cases/search_api_settings_use_cases.dart
@@ -0,0 +1,13 @@
+// import 'package:home_decor_app/core/networks/api_resulte.dart';
+// import 'package:home_decor_app/features/search/domain/entity/search_api_settings_response_model_entity.dart';
+// import 'package:home_decor_app/features/search/domain/repo/search_repo.dart';
+
+// class SearchApiSettingsUseCases {
+//   final SearchRepo _searchRepo;
+//   SearchApiSettingsUseCases(this._searchRepo);
+
+//   Future<ApiResulte<SearchApiSettingsResponseModelEntity>>
+//   getApiSettings() async {
+//     return await _searchRepo.getApiSettings();
+//   }
+// }
