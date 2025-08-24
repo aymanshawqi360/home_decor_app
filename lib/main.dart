a8e915ae3332c632d5c202593955dbe86525bdf6
aymanshawqi360 <aymanshawqi380gmail.com>
2025-08-25 00:49:48 +0300

    - added a file main

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:home_decor_app/core/di/dependency_injection.dart';
+import 'package:home_decor_app/core/helper/app_bloc_ob_server.dart';
 import 'package:home_decor_app/core/helper/extensions.dart';
 import 'package:home_decor_app/core/networks/api_constants.dart';
-import 'package:home_decor_app/core/networks/app_string.dart';
+import 'package:home_decor_app/core/helper/app_string.dart';
 import 'package:home_decor_app/core/networks/save_the_token.dart';
 import 'package:home_decor_app/core/routes/app_router.dart';
 import 'package:home_decor_app/home_decor_app.dart';
@@ -15,6 +17,7 @@ void main() async {
     DeviceOrientation.portraitDown,
   ]);
   setupGetIt();
+  Bloc.observer = AppBlocObserver();
   await checkIfloggedInUser();
   runApp(HomeDecorApp(AppRouter()));
 }
