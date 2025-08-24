64bd79b1adb83ad25975679a44f4b787bedd9648
aymanshawqi360 <aymanshawqi380gmail.com>
2025-08-25 01:38:59 +0300

    - added amendment on page favorite

--- lib/features/favorite/presentation/pages/favorite_screen.dart
+++ lib/features/favorite/presentation/pages/favorite_screen.dart
@@ -1,11 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_svg/svg.dart';
-import 'package:home_decor_app/core/helper/app_assets.dart';
-import 'package:home_decor_app/core/helper/app_string.dart';
-import 'package:home_decor_app/core/helper/extensions.dart';
-import 'package:home_decor_app/core/theme/styles.dart';
-import 'package:home_decor_app/features/favorite/presentation/cubit/cubit/favorite_cubit.dart';
+import 'package:home_decor_app/features/favorite/presentation/widget/appbar_favorite.dart';
 import 'package:home_decor_app/features/favorite/presentation/widget/favorite_bloc_listener.dart';
 
 class FavoriteScreen extends StatelessWidget {
@@ -13,29 +7,6 @@ class FavoriteScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        surfaceTintColor: Colors.transparent,
-        actions: [
-          GestureDetector(
-            onTap: () {
-              context.read<FavoriteCubit>().checkIconDelete();
-            },
-            child: SvgPicture.asset(
-              AppAssets.imageAndSvg.deleteIcon,
-              height: context.screenHeight / 35,
-              width: context.screenWidth / 35,
-            ),
-          ),
-        ],
-        actionsPadding: EdgeInsets.only(right: context.screenWidth / 20),
-        title: Text(
-          AppString.favorite,
-          style: TextStyles.font20LigthBrownSemiBold,
-        ),
-        centerTitle: true,
-      ),
-      body: FavoriteBlocListener(),
-    );
+    return Scaffold(appBar: AppBarFavorite(), body: FavoriteBlocListener());
   }
 }
