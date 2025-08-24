daccd5c66df0e07ab143be147289516cc0315290
aymanshawqi360 <aymanshawqi380gmail.com>
2025-08-25 01:41:37 +0300

    - added amendment on page button_navigate_between_screens

--- lib/features/home/presentation/pages/button_navigate_between_screen.dart
+++ lib/features/home/presentation/pages/button_navigate_between_screen.dart
@@ -6,7 +6,7 @@ import 'package:home_decor_app/core/di/dependency_injection.dart';
 import 'package:home_decor_app/core/helper/app_assets.dart';
 import 'package:home_decor_app/core/helper/spacing.dart';
 import 'package:home_decor_app/core/theme/colors.dart';
-import 'package:home_decor_app/features/favorite/presentation/page/favorite_screen.dart';
+import 'package:home_decor_app/features/favorite/presentation/pages/favorite_screen.dart';
 import 'package:home_decor_app/features/home/presentation/cubit/best_seller/best_seller_cubit.dart';
 import 'package:home_decor_app/features/home/presentation/cubit/categories/categories_cubit.dart';
 import 'package:home_decor_app/features/home/presentation/cubit/cubit/slider_cubit.dart';
