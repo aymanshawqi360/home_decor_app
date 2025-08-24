c3e7e5e317ab4bd15a45894905fde310320e320b
aymanshawqi360 <aymanshawqi380gmail.com>
2025-08-25 00:22:47 +0300

    - added a file button_navigate_between_screen

--- lib/features/home/presentation/pages/button_navigate_between_screen.dart
+++ lib/features/home/presentation/pages/button_navigate_between_screen.dart
@@ -6,6 +6,7 @@ import 'package:home_decor_app/core/di/dependency_injection.dart';
 import 'package:home_decor_app/core/helper/app_assets.dart';
 import 'package:home_decor_app/core/helper/spacing.dart';
 import 'package:home_decor_app/core/theme/colors.dart';
+import 'package:home_decor_app/features/favorite/presentation/page/favorite_screen.dart';
 import 'package:home_decor_app/features/home/presentation/cubit/best_seller/best_seller_cubit.dart';
 import 'package:home_decor_app/features/home/presentation/cubit/categories/categories_cubit.dart';
 import 'package:home_decor_app/features/home/presentation/cubit/cubit/slider_cubit.dart';
@@ -25,19 +26,23 @@ class _ButtonNavigateBetweenScreenState
   List<Widget> pages = [
     MultiBlocProvider(
       providers: [
-        BlocProvider(create: (context) => SliderCubit(getIt())..getSlider()),
+        BlocProvider(create: (context) => getIt<SliderCubit>()..getSlider()),
+
         BlocProvider(
-          create: (context) => CategoriesCubit(getIt())..getCategories(),
+          create: (context) => getIt<CategoriesCubit>()..getCategories(),
         ),
-        BlocProvider(create: (context) => BestSellerCubit(getIt())),
         BlocProvider(
-          create: (context) => NewCollectionCubit(getIt())..getNewCollection(),
+          create: (context) => getIt<BestSellerCubit>()..getBestSellet(),
+        ),
+
+        BlocProvider(
+          create: (context) => getIt<NewCollectionCubit>()..getNewCollection(),
         ),
       ],
       child: HomeScreen(),
     ),
     Container(color: Colors.amber),
-    Container(color: Colors.red),
+    FavoriteScreen(),
     Container(color: Colors.green),
     Container(color: Colors.blue),
   ];
@@ -46,8 +51,6 @@ class _ButtonNavigateBetweenScreenState
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: pages[index],
-
       bottomNavigationBar: Container(
         height: 58.h,
         color: Colors.white,
@@ -62,6 +65,7 @@ class _ButtonNavigateBetweenScreenState
           ],
         ),
       ),
+      body: pages[index],
     );
   }
 
@@ -79,13 +83,13 @@ class _ButtonNavigateBetweenScreenState
                 mainAxisAlignment: MainAxisAlignment.center,
                 crossAxisAlignment: CrossAxisAlignment.center,
                 children: [
-                  SvgPicture.asset(image, color: ColorsMananger.darkBrown),
+                  SvgPicture.asset(image, color: ColorsManager.darkBrown),
                   verticalSpace(5),
                   Container(
                     width: MediaQuery.sizeOf(context).width / 22,
                     height: MediaQuery.sizeOf(context).height / 350,
                     decoration: BoxDecoration(
-                      color: ColorsMananger.darkBrown,
+                      color: ColorsManager.darkBrown,
                       borderRadius: BorderRadius.circular(5.r),
                     ),
                   ),
