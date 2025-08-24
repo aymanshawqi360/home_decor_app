f427649c0ca660c420a4148ad8a64ba17c446bac
aymanshawqi360 <aymanshawqi380gmail.com>
2025-08-25 00:09:35 +0300

    - added a file app_router

--- lib/core/routes/app_router.dart
+++ lib/core/routes/app_router.dart
@@ -2,14 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:home_decor_app/core/di/dependency_injection.dart';
 import 'package:home_decor_app/core/routes/routes.dart';
+import 'package:home_decor_app/features/favorite/presentation/cubit/cubit/favorite_cubit.dart';
 import 'package:home_decor_app/features/forgot_password/presentation/pages/forgot_password_screen.dart';
 import 'package:home_decor_app/features/home/presentation/pages/button_navigate_between_screen.dart';
-import 'package:home_decor_app/features/home/presentation/pages/home_screen.dart';
 import 'package:home_decor_app/features/login/presentation/cubit/login_cubit.dart';
 import 'package:home_decor_app/features/login/presentation/pages/login_screen.dart';
 import 'package:home_decor_app/features/onboarding/presentation/pages/onboarding_screen.dart';
-import 'package:home_decor_app/features/search/presentation/cubit/search_cubit.dart';
-import 'package:home_decor_app/features/search/presentation/pages/search_screen.dart';
 import 'package:home_decor_app/features/set_password/presentation/pages/set_password_screen.dart';
 import 'package:home_decor_app/features/sign_up_screen/presentation/cubit/cubit/sign_up_cubit.dart';
 import 'package:home_decor_app/features/sign_up_screen/presentation/pages/sign_up_screen.dart';
@@ -24,7 +22,13 @@ class AppRouter {
           },
         );
       case Routes.buttonNavigateBetweenScreen:
-        return MaterialPageRoute(builder: (_) => ButtonNavigateBetweenScreen());
+        return MaterialPageRoute(
+          builder:
+              (_) => BlocProvider(
+                create: (context) => getIt<FavoriteCubit>()..getListFavorite(),
+                child: ButtonNavigateBetweenScreen(),
+              ),
+        );
       case Routes.login:
         return MaterialPageRoute(
           builder:
@@ -45,21 +49,6 @@ class AppRouter {
         return MaterialPageRoute(builder: (_) => ForgotPasswordScreen());
       case Routes.setPassword:
         return MaterialPageRoute(builder: (_) => SetPasswordScreen());
-      case Routes.homeScreen:
-        return MaterialPageRoute(builder: (_) => HomeScreen());
-      case Routes.search:
-        return MaterialPageRoute(
-          builder:
-              (_) => BlocProvider(
-                create:
-                    (context) =>
-                        SearchCubit(getIt())
-                          ..getFilterResult()
-                          ..getApiSettings(),
-
-                child: SearchScreen(),
-              ),
-        );
 
       default:
         return null;
