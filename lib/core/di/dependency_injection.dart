2a3f6a5e62421b1663ea34bd9536d981bee94aee
aymanshawqi360 <aymanshawqi380gmail.com>
2025-08-24 23:58:49 +0300

    - added dependency_injection

--- lib/core/di/dependency_injection.dart
+++ lib/core/di/dependency_injection.dart
@@ -1,6 +1,11 @@
 import 'package:dio/dio.dart';
 import 'package:get_it/get_it.dart';
 import 'package:home_decor_app/core/networks/dio_factory.dart';
+import 'package:home_decor_app/features/favorite/data/api/favorite_api_service.dart';
+import 'package:home_decor_app/features/favorite/data/repo_implementation/favorite_repo_implementation.dart';
+import 'package:home_decor_app/features/favorite/domain/repo/favorite_repo.dart';
+import 'package:home_decor_app/features/favorite/domain/use_cases/favorite_api_use_cases.dart';
+import 'package:home_decor_app/features/favorite/presentation/cubit/cubit/favorite_cubit.dart';
 import 'package:home_decor_app/features/home/data/apis/home_api_service.dart';
 import 'package:home_decor_app/features/home/data/repo_implementation/home_repo_implementation.dart';
 import 'package:home_decor_app/features/home/domain/repo/home_repo.dart';
@@ -8,6 +13,10 @@ import 'package:home_decor_app/features/home/domain/use_cases/best_seller_use_ca
 import 'package:home_decor_app/features/home/domain/use_cases/categories_use_cases.dart';
 import 'package:home_decor_app/features/home/domain/use_cases/new_collection_use_cases.dart';
 import 'package:home_decor_app/features/home/domain/use_cases/slider_use_cases.dart';
+import 'package:home_decor_app/features/home/presentation/cubit/best_seller/best_seller_cubit.dart';
+import 'package:home_decor_app/features/home/presentation/cubit/categories/categories_cubit.dart';
+import 'package:home_decor_app/features/home/presentation/cubit/cubit/slider_cubit.dart';
+import 'package:home_decor_app/features/home/presentation/cubit/new_collection/new_collection_cubit.dart';
 import 'package:home_decor_app/features/login/data/api/login_api_service.dart';
 import 'package:home_decor_app/features/login/data/repo_implementation/login_repo_implementation.dart';
 import 'package:home_decor_app/features/login/domain/repo/login_repo.dart';
@@ -24,44 +33,53 @@ import 'package:home_decor_app/features/sign_up_screen/domain/use_cases/sign_up_
 final getIt = GetIt.instance;
 
 Future<void> setupGetIt() async {
+  //!Core
   Dio dio = DioFactory.getDio();
+  //!Data Layer
   getIt.registerLazySingleton<HomeApiService>(() => HomeApiService(dio));
-  getIt.registerLazySingleton<HomeRepo>(() => HomeRepoImplementation(getIt()));
-
-  //Slider
-  getIt.registerLazySingleton<SliderUseCases>(() => SliderUseCases(getIt()));
-  //Categories
-  getIt.registerLazySingleton<CategoriesUseCases>(
-    () => CategoriesUseCases(getIt()),
-  );
-  //Best-Seller
-  getIt.registerLazySingleton<BestSellerUseCases>(
-    () => BestSellerUseCases(getIt()),
-  );
-
-  //NewCollection
-  getIt.registerLazySingleton<NewCollectionUseCases>(
-    () => NewCollectionUseCases(getIt()),
-  );
-
-  //SignUp
-
-  getIt.registerLazySingleton<SignUpApiService>(() => SignUpApiService(dio));
-  getIt.registerLazySingleton<SignUpRepo>(() => RepoImplementation(getIt()));
-  getIt.registerLazySingleton<SignUpUseCases>(() => SignUpUseCases(getIt()));
-
-  //Login
-
   getIt.registerLazySingleton<LoginApiService>(() => LoginApiService(dio));
-  getIt.registerLazySingleton<LoginRepo>(
-    () => LoginRepoImplementation(getIt()),
-  );
-  getIt.registerLazySingleton<LoginUseCase>(() => LoginUseCase(getIt()));
-
-  //Search
   getIt.registerLazySingleton<SearchApiService>(() => SearchApiService(dio));
+  getIt.registerLazySingleton<SignUpApiService>(() => SignUpApiService(dio));
+  getIt.registerLazySingleton<FavoriteApiService>(
+    () => FavoriteApiService(dio),
+  );
+
+  //!Repo Layer
   getIt.registerLazySingleton<SearchRepo>(
     () => SearchRepoImplementation(getIt()),
   );
+  getIt.registerLazySingleton<HomeRepo>(() => HomeRepoImplementation(getIt()));
+  getIt.registerLazySingleton<SignUpRepo>(() => RepoImplementation(getIt()));
+  getIt.registerLazySingleton<LoginRepo>(
+    () => LoginRepoImplementation(getIt()),
+  );
+  getIt.registerLazySingleton<FavoriteRepo>(
+    () => FavoriteRepoImplementation(favoriteApiService: getIt()),
+  );
+
+  //!UseCases
+  getIt.registerLazySingleton<LoginUseCase>(() => LoginUseCase(getIt()));
+  getIt.registerLazySingleton<SignUpUseCases>(() => SignUpUseCases(getIt()));
+  getIt.registerLazySingleton<BestSellerUseCases>(
+    () => BestSellerUseCases(getIt()),
+  );
+  getIt.registerLazySingleton<CategoriesUseCases>(
+    () => CategoriesUseCases(getIt()),
+  );
+  getIt.registerLazySingleton<SliderUseCases>(() => SliderUseCases(getIt()));
+  getIt.registerLazySingleton<NewCollectionUseCases>(
+    () => NewCollectionUseCases(getIt()),
+  );
   getIt.registerLazySingleton<SearchUseCases>(() => SearchUseCases(getIt()));
+  getIt.registerLazySingleton<FavoriteApiUseCases>(
+    () => FavoriteApiUseCases(favoriteRepo: getIt()),
+  );
+
+  //!Bloc
+
+  getIt.registerFactory(() => SliderCubit(sliderUseCases: getIt()));
+  getIt.registerFactory(() => CategoriesCubit(getIt()));
+  getIt.registerFactory(() => BestSellerCubit(getIt()));
+  getIt.registerFactory(() => NewCollectionCubit(getIt()));
+  getIt.registerFactory(() => FavoriteCubit(favoriteApiUseCases: getIt()));
 }
